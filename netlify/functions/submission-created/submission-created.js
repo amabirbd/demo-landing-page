6d9f2a8fb76e4e6f806d02ab67c2e5878dee36b4
amabir <amabirbd@gmail.com>
2022-05-30 23:07:47 +0600

    fix: somefix

--- netlify/functions/submission-created/submission-created.js
+++ netlify/functions/submission-created/submission-created.js
@@ -14,44 +14,53 @@ import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 //   FIREBASE_MEASUREMENT_ID,
 // } = process.env;
 
+const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY;
+const FIREBASE_AUTH_DOMAIN = process.env.FIREBASE_AUTH_DOMAIN;
+const FIREBASE_DATABASE_URL = process.env.FIREBASE_DATABASE_URL;
+const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
+const FIREBASE_STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET;
+const FIREBASE_MESSAGING_SENDER_ID = process.env.FIREBASE_MESSAGING_SENDER_ID;
+const FIREBASE_APP_ID = process.env.FIREBASE_APP_ID;
+const FIREBASE_MEASUREMENT_ID = process.env.FIREBASE_MEASUREMENT_ID;
+
 exports.handler = async (event) => {
   const data = JSON.parse(event.body).payload;
   const { name, email, message } = data.data;
   console.log(data);
 
-  // const firebaseConfig = {
-  //   apiKey: FIREBASE_API_KEY,
-  //   authDomain: FIREBASE_AUTH_DOMAIN,
-  //   databaseURL: FIREBASE_DATABASE_URL,
-  //   projectId: FIREBASE_PROJECT_ID,
-  //   storageBucket: FIREBASE_STORAGE_BUCKET,
-  //   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  //   appId: FIREBASE_APP_ID,
-  //   measurementId: FIREBASE_MEASUREMENT_ID,
-  // };
+  const firebaseConfig = {
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    databaseURL: FIREBASE_DATABASE_URL,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
+    measurementId: FIREBASE_MEASUREMENT_ID,
+  };
 
-  // // init firebase
-  // initializeApp(firebaseConfig);
+  // init firebase
+  initializeApp(firebaseConfig);
 
-  // // init services
-  // const db = getFirestore();
+  // init services
+  const db = getFirestore();
 
-  // // collection ref
-  // const colRef = collection(db, "messages");
+  // collection ref
+  const colRef = collection(db, "messages");
 
-  // // get collection data
-  // getDocs(colRef)
-  //   .then((snapshot) => {
-  //     // console.log(snapshot.docs)
-  //     let messages = [];
-  //     snapshot.docs.forEach((doc) => {
-  //       messages.push({ ...doc.data(), id: doc.id });
-  //     });
-  //     console.log(messages);
-  //   })
-  //   .catch((err) => {
-  //     console.log(err.message);
-  //   });
+  // get collection data
+  getDocs(colRef)
+    .then((snapshot) => {
+      // console.log(snapshot.docs)
+      let messages = [];
+      snapshot.docs.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(messages);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 
   // // add contact message
   // const addContactMessageForm = document.querySelector(".contact-form");
