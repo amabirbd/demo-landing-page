808386137d7fae12daa5d364a380ff84d6dbe63d
amabir <amabirbd@gmail.com>
2022-05-30 19:57:43 +0600

    feat

--- netlify/functions/submission-created/submission-created.js
+++ netlify/functions/submission-created/submission-created.js
@@ -1,75 +1,73 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 
-require("dotenv").config();
+// require("dotenv").config();
 
-const {
-  FIREBASE_API_KEY,
-  FIREBASE_AUTH_DOMAIN,
-  FIREBASE_DATABASE_URL,
-  FIREBASE_PROJECT_ID,
-  FIREBASE_STORAGE_BUCKET,
-  FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_APP_ID,
-  FIREBASE_MEASUREMENT_ID,
-} = process.env;
+// const {
+//   FIREBASE_API_KEY,
+//   FIREBASE_AUTH_DOMAIN,
+//   FIREBASE_DATABASE_URL,
+//   FIREBASE_PROJECT_ID,
+//   FIREBASE_STORAGE_BUCKET,
+//   FIREBASE_MESSAGING_SENDER_ID,
+//   FIREBASE_APP_ID,
+//   FIREBASE_MEASUREMENT_ID,
+// } = process.env;
 
 exports.handler = async (event) => {
   const data = JSON.parse(event.body).payload;
   const { name, email, message } = data.data;
-  console.log(name, email, message);
+  console.log(data);
 
-  const firebaseConfig = {
-    apiKey: FIREBASE_API_KEY,
-    authDomain: FIREBASE_AUTH_DOMAIN,
-    databaseURL: FIREBASE_DATABASE_URL,
-    projectId: FIREBASE_PROJECT_ID,
-    storageBucket: FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-    appId: FIREBASE_APP_ID,
-    measurementId: FIREBASE_MEASUREMENT_ID,
-  };
+  // const firebaseConfig = {
+  //   apiKey: FIREBASE_API_KEY,
+  //   authDomain: FIREBASE_AUTH_DOMAIN,
+  //   databaseURL: FIREBASE_DATABASE_URL,
+  //   projectId: FIREBASE_PROJECT_ID,
+  //   storageBucket: FIREBASE_STORAGE_BUCKET,
+  //   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  //   appId: FIREBASE_APP_ID,
+  //   measurementId: FIREBASE_MEASUREMENT_ID,
+  // };
 
-  // init firebase
-  initializeApp(firebaseConfig);
+  // // init firebase
+  // initializeApp(firebaseConfig);
 
-  // init services
-  const db = getFirestore();
+  // // init services
+  // const db = getFirestore();
 
-  // collection ref
-  const colRef = collection(db, "messages");
+  // // collection ref
+  // const colRef = collection(db, "messages");
 
-  // get collection data
-  getDocs(colRef)
-    .then((snapshot) => {
-      // console.log(snapshot.docs)
-      let messages = [];
-      snapshot.docs.forEach((doc) => {
-        messages.push({ ...doc.data(), id: doc.id });
-      });
-      console.log(messages);
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+  // // get collection data
+  // getDocs(colRef)
+  //   .then((snapshot) => {
+  //     // console.log(snapshot.docs)
+  //     let messages = [];
+  //     snapshot.docs.forEach((doc) => {
+  //       messages.push({ ...doc.data(), id: doc.id });
+  //     });
+  //     console.log(messages);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err.message);
+  //   });
 
-  // add contact message
-  const addContactMessageForm = document.querySelector(".contact-form");
+  // // add contact message
+  // const addContactMessageForm = document.querySelector(".contact-form");
 
-  console.log(addContactMessageForm);
+  // console.log(addContactMessageForm);
 
-  addContactMessageForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log(addContactMessageForm);
+  // addContactMessageForm.addEventListener("submit", (e) => {
+  //   e.preventDefault();
+  //   console.log(addContactMessageForm);
 
-    addDoc(colRef, {
-      name: addContactMessageForm.fullname.value,
-      email: addContactMessageForm.email.value,
-      message: addContactMessageForm.message.value,
-    }).then(() => {
-      addContactMessageForm.reset();
-    });
-  });
+  //   addDoc(colRef, {
+  //     name: addContactMessageForm.fullname.value,
+  //     email: addContactMessageForm.email.value,
+  //     message: addContactMessageForm.message.value,
+  //   });
+  // });
 
   return {
     statusCode: 200,
