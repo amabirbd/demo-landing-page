d67e3202e56b14545812adf36990f52f115e0f74
amabir <amabirbd@gmail.com>
2022-06-01 11:03:16 +0600

    fix

--- netlify/functions/submission-created/submission-created.js
+++ netlify/functions/submission-created/submission-created.js
@@ -30,33 +30,33 @@ const FIREBASE_MESSAGING_SENDER_ID = process.env.FIREBASE_MESSAGING_SENDER_ID;
 const FIREBASE_APP_ID = process.env.FIREBASE_APP_ID;
 const FIREBASE_MEASUREMENT_ID = process.env.FIREBASE_MEASUREMENT_ID;
 
-const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  databaseURL: FIREBASE_DATABASE_URL,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID,
-  measurementId: FIREBASE_MEASUREMENT_ID,
-};
+// const firebaseConfig = {
+//   apiKey: FIREBASE_API_KEY,
+//   authDomain: FIREBASE_AUTH_DOMAIN,
+//   databaseURL: FIREBASE_DATABASE_URL,
+//   projectId: FIREBASE_PROJECT_ID,
+//   storageBucket: FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+//   appId: FIREBASE_APP_ID,
+//   measurementId: FIREBASE_MEASUREMENT_ID,
+// };
 
 exports.handler = async (event) => {
   const data = JSON.parse(event.body).payload.data;
   const { fullname, email, message } = data;
   console.log(fullname, email, message);
 
-  // const firebaseConfig = {
-  //   apiKey: "AIzaSyCRG3egud11OW0TNX917LpM2j2fAEfThIw",
-  //   authDomain: "software-sheba-emails.firebaseapp.com",
-  //   databaseURL:
-  //     "https://software-sheba-emails-default-rtdb.asia-southeast1.firebasedatabase.app",
-  //   projectId: "software-sheba-emails",
-  //   storageBucket: "software-sheba-emails.appspot.com",
-  //   messagingSenderId: "157322159291",
-  //   appId: "1:157322159291:web:89ae77bed4ddd052d9b59e",
-  //   measurementId: "G-EM3M43GQDD",
-  // };
+  const firebaseConfig = {
+    apiKey: "AIzaSyCRG3egud11OW0TNX917LpM2j2fAEfThIw",
+    authDomain: "software-sheba-emails.firebaseapp.com",
+    databaseURL:
+      "https://software-sheba-emails-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "software-sheba-emails",
+    storageBucket: "software-sheba-emails.appspot.com",
+    messagingSenderId: "157322159291",
+    appId: "1:157322159291:web:89ae77bed4ddd052d9b59e",
+    measurementId: "G-EM3M43GQDD",
+  };
 
   // init firebase
   initializeApp(firebaseConfig);
