90fdffc6d6c4d9b647b70dc5b65e735cebfd68cb
amabir <amabirbd@gmail.com>
2022-06-01 10:56:06 +0600

    fix

--- netlify/functions/submission-created/submission-created.js
+++ netlify/functions/submission-created/submission-created.js
@@ -1,12 +1,12 @@
-// import { initializeApp } from "firebase/app";
-// import {
-//   getFirestore,
-//   collection,
-//   getDocs,
-//   setDoc,
-//   setLogLevel,
-//   addDoc,
-// } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  setDoc,
+  setLogLevel,
+  addDoc,
+} from "firebase/firestore";
 
 // require("dotenv").config();
 
@@ -21,25 +21,25 @@
 //   FIREBASE_MEASUREMENT_ID,
 // } = process.env;
 
-// const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY;
-// const FIREBASE_AUTH_DOMAIN = process.env.FIREBASE_AUTH_DOMAIN;
-// const FIREBASE_DATABASE_URL = process.env.FIREBASE_DATABASE_URL;
-// const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
-// const FIREBASE_STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET;
-// const FIREBASE_MESSAGING_SENDER_ID = process.env.FIREBASE_MESSAGING_SENDER_ID;
-// const FIREBASE_APP_ID = process.env.FIREBASE_APP_ID;
-// const FIREBASE_MEASUREMENT_ID = process.env.FIREBASE_MEASUREMENT_ID;
+const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY;
+const FIREBASE_AUTH_DOMAIN = process.env.FIREBASE_AUTH_DOMAIN;
+const FIREBASE_DATABASE_URL = process.env.FIREBASE_DATABASE_URL;
+const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
+const FIREBASE_STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET;
+const FIREBASE_MESSAGING_SENDER_ID = process.env.FIREBASE_MESSAGING_SENDER_ID;
+const FIREBASE_APP_ID = process.env.FIREBASE_APP_ID;
+const FIREBASE_MEASUREMENT_ID = process.env.FIREBASE_MEASUREMENT_ID;
 
-// const firebaseConfig = {
-//   apiKey: FIREBASE_API_KEY,
-//   authDomain: FIREBASE_AUTH_DOMAIN,
-//   databaseURL: FIREBASE_DATABASE_URL,
-//   projectId: FIREBASE_PROJECT_ID,
-//   storageBucket: FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-//   appId: FIREBASE_APP_ID,
-//   measurementId: FIREBASE_MEASUREMENT_ID,
-// };
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  databaseURL: FIREBASE_DATABASE_URL,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
+};
 
 exports.handler = async (event) => {
   const data = JSON.parse(event.body).payload.data;
@@ -58,29 +58,29 @@ exports.handler = async (event) => {
   //   measurementId: "G-EM3M43GQDD",
   // };
 
-  // // init firebase
-  // initializeApp(firebaseConfig);
+  // init firebase
+  initializeApp(firebaseConfig);
 
-  // // init services
-  // const db = getFirestore();
+  // init services
+  const db = getFirestore();
 
-  // // collection ref
-  // const colRef = collection(db, "messages");
+  // collection ref
+  const colRef = collection(db, "messages");
 
-  // addDoc(colRef, {
-  //   name: fullname,
-  //   email: email,
-  //   message: message,
-  // })
-  //   .then(() => {
-  //     console.log("success firebase");
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
+  addDoc(colRef, {
+    name: fullname,
+    email: email,
+    message: message,
+  })
+    .then(() => {
+      console.log("success firebase");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
-  // return {
-  //   statusCode: 200,
-  //   body: JSON.stringify({ status: "ok" }),
-  // };
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ status: "ok" }),
+  };
 };
