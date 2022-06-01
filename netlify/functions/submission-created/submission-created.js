af3e1a82e38b213c66f7f675aadf3d0bb5a8bbd8
amabir <amabirbd@gmail.com>
2022-06-01 11:25:32 +0600

    fix: firebase integrated

--- netlify/functions/submission-created/submission-created.js
+++ netlify/functions/submission-created/submission-created.js
@@ -8,18 +8,27 @@ import {
   addDoc,
 } from "firebase/firestore";
 
-require("dotenv").config();
+// require("dotenv").config();
 
-const {
-  FIREBASE_API_KEY,
-  FIREBASE_AUTH_DOMAIN,
-  FIREBASE_DATABASE_URL,
-  FIREBASE_PROJECT_ID,
-  FIREBASE_STORAGE_BUCKET,
-  FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_APP_ID,
-  FIREBASE_MEASUREMENT_ID,
-} = process.env;
+// const {
+//   FIREBASE_API_KEY,
+//   FIREBASE_AUTH_DOMAIN,
+//   FIREBASE_DATABASE_URL,
+//   FIREBASE_PROJECT_ID,
+//   FIREBASE_STORAGE_BUCKET,
+//   FIREBASE_MESSAGING_SENDER_ID,
+//   FIREBASE_APP_ID,
+//   FIREBASE_MEASUREMENT_ID,
+// } = process.env;
+
+const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY;
+const FIREBASE_AUTH_DOMAIN = process.env.FIREBASE_AUTH_DOMAIN;
+const FIREBASE_DATABASE_URL = process.env.FIREBASE_DATABASE_URL;
+const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
+const FIREBASE_STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET;
+const FIREBASE_MESSAGING_SENDER_ID = process.env.FIREBASE_MESSAGING_SENDER_ID;
+const FIREBASE_APP_ID = process.env.FIREBASE_APP_ID;
+const FIREBASE_MEASUREMENT_ID = process.env.FIREBASE_MEASUREMENT_ID;
 
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
@@ -36,18 +45,6 @@ exports.handler = async (event) => {
   const data = JSON.parse(event.body).payload.data;
   const { fullname, email, message } = data;
   console.log(fullname, email, message);
-
-  // const firebaseConfig = {
-  //   apiKey: "AIzaSyCRG3egud11OW0TNX917LpM2j2fAEfThIw",
-  //   authDomain: "software-sheba-emails.firebaseapp.com",
-  //   databaseURL:
-  //     "https://software-sheba-emails-default-rtdb.asia-southeast1.firebasedatabase.app",
-  //   projectId: "software-sheba-emails",
-  //   storageBucket: "software-sheba-emails.appspot.com",
-  //   messagingSenderId: "157322159291",
-  //   appId: "1:157322159291:web:89ae77bed4ddd052d9b59e",
-  //   measurementId: "G-EM3M43GQDD",
-  // };
 
   // init firebase
   initializeApp(firebaseConfig);
